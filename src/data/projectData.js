d56343a6b229f8346dea3d6a0b1f167faa9a1044
abdullahyilmazkol <58661477+abdullahyilmazkol@users.noreply.github.com>
2024-10-14 14:02:29 +0300

    Update projectData.js

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,9 +3,9 @@ export const projects = [
     {
         id: 1,
         title: 'Proje 1',
-        image: '../Uploads/1.jpeg',
+        image: './Uploads/1.jpeg',
         description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        gallery: ['../Uploads/1.jpeg', '../Uploads/1.jpeg', '../Uploads/1.jpeg', '../Uploads/1.jpeg','../Uploads/1.jpeg', '../Uploads/1.jpeg'],
+        gallery: ['./Uploads/1.jpeg', '../Uploads/1.jpeg', '../Uploads/1.jpeg', '../Uploads/1.jpeg','../Uploads/1.jpeg', '../Uploads/1.jpeg'],
       },
       {
         id: 2,
